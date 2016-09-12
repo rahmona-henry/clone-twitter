1b3f6d1c7f2e1bb79db1cf932d1969afccf9061c
rahmona-henry <rahmona.henry@icloud.com>
2016-09-12 21:03:37 +1200

    Adjust server file

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-var app = require('./app')
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL || {
+    database: 'twitter_clone_dev'
+  },
+  useNullAsDefault: true
+})
+
+var app = require('./app')(knex)
+var port = process.env.PORT || 3000
 
 app.listen(3000, function() {
   console.log('Get tweeting on port 3000!')
