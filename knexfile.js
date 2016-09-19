25b82d56f6992c3b64a40225b607b5e61eba14d4
rahmona-henry <rahmona.henry@icloud.com>
2016-09-19 22:10:02 +1200

    configure the knexfile

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'twitter_clone_dev'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      database: 'twitter_clone_dev',
+    }
+    },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
